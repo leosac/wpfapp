d64432bcb74ce9fbbc42fbaad33c13e0d7574fd9
Maxhy <maxime@leosac.com>
2023-04-25 16:05:47 +0200

    Move Async Command from LKM to WpfApp project

---
+++ WpfApp/Domain/ILeosacAppErrorHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leosac.WpfApp.Domain
+{
+    public interface ILeosacAppErrorHandler
+    {
+        void HandleError(Exception ex);
+    }
+}
