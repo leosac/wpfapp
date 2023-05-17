e8a895d095fb55d534f77c0336360cd5bb605436
Maxhy <maxime.chamley@leosac.com>
2023-05-17 09:45:35 +0200

    Move additional converters from LKM

---
+++ WpfApp/Domain/ByteArrayToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Leosac.WpfApp.Domain
+{
+    public class ByteArrayToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value is byte[] v)
+                return System.Convert.ToHexString(v);
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value is string v)
+                return System.Convert.FromHexString(v);
+
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ WpfApp/Domain/ByteLengthToCharLengthConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Leosac.WpfApp.Domain
+{
+    public class ByteLengthToCharLengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Binding.DoNothing;
+
+            var length = System.Convert.ToUInt32(value);
+            return length * 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Binding.DoNothing;
+
+            var length = System.Convert.ToUInt32(value);
+            return (uint)(length / 2);
+        }
+    }
+}
