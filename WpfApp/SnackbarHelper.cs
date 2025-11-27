0fa77484135e3dbe701e1e4bed5280a65450ea76
Maxime C. <maxime@leosac.com>
2025-11-27 11:12:49 +0100

    Add option to override message queue duration

--- WpfApp/SnackbarHelper.cs
+++ WpfApp/SnackbarHelper.cs
@@ -8,17 +8,17 @@ namespace Leosac.WpfApp
 {
     public static class SnackbarHelper
     {
-        public static void EnqueueError(ISnackbarMessageQueue? queue, string message)
+        public static void EnqueueError(ISnackbarMessageQueue? queue, string message, int duration = 5)
         {
-            EnqueueError(queue, null, message);
+            EnqueueError(queue, null, message, duration);
         }
 
-        public static void EnqueueError(ISnackbarMessageQueue? queue, Exception? ex)
+        public static void EnqueueError(ISnackbarMessageQueue? queue, Exception? ex, int duration = 5)
         {
-            EnqueueError(queue, ex, null);
+            EnqueueError(queue, ex, null, duration);
         }
 
-        public static void EnqueueError(ISnackbarMessageQueue? queue, Exception? ex, string? message)
+        public static void EnqueueError(ISnackbarMessageQueue? queue, Exception? ex, string? message, int duration = 5)
         {
             if (ex != null)
             {
@@ -43,17 +43,17 @@ namespace Leosac.WpfApp
             panel.Children.Add(errorIcon);
             panel.Children.Add(new TextBlock() { Text = message, Margin = new Thickness(5, 0, 0 ,0), TextWrapping = TextWrapping.Wrap });
 
-            EnqueueMessage(queue, panel);
+            EnqueueMessage(queue, panel, duration);
         }
 
-        public static void EnqueueMessage(ISnackbarMessageQueue? queue, PackIconKind icon, object message)
+        public static void EnqueueMessage(ISnackbarMessageQueue? queue, PackIconKind icon, object message, int duration)
         {
-            queue?.Enqueue(message, new PackIcon { Kind = icon }, (object? _) => { }, null, false, true, TimeSpan.FromSeconds(5));
+            queue?.Enqueue(message, new PackIcon { Kind = icon }, (object? _) => { }, null, false, true, duration > 0 ? TimeSpan.FromSeconds(duration) : null);
         }
 
-        public static void EnqueueMessage(ISnackbarMessageQueue? queue, object message)
+        public static void EnqueueMessage(ISnackbarMessageQueue? queue, object message, int duration = 5)
         {
-            EnqueueMessage(queue, PackIconKind.CloseBold, message);
+            EnqueueMessage(queue, PackIconKind.CloseBold, message, duration);
         }
 
         public static void HandlePreviewMouseWheel(object sender, MouseWheelEventArgs e)
